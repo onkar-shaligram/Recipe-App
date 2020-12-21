0393cdf8d5a920a39f5c983bb2d09f1d88feffe0
onkar-shaligram <onkarshaligram01@gmail.com>
2020-12-22 00:18:07 +0530

    App completed

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -16,31 +16,43 @@ class _HomeState extends State<Home> {
 
   List<Recipe> recipes = List<Recipe>();
 
+  String ingredient;
+
+  bool loading = false;
+
   getRecipes() async {
 
-    
+    setState(() {
+      loading = true;
+    });
 
-    var recipeData = await http.get("https://api.edamam.com/search?q=apple&app_id=$apiId&app_key=$apiKey");
+    var recipeData = await http.get("https://api.edamam.com/search?q=$ingredient&app_id=$apiId&app_key=$apiKey");
 
     Map<String,dynamic> jsonData = jsonDecode(recipeData.body);
     print(jsonData);
 
     jsonData["hits"].forEach((recipeInfo) {
       Recipe recipe = Recipe();
-      recipe = Recipe.fromMap(recipeInfo["recipe"]);
 
+      Map<String, dynamic> recipeInfo2 = recipeInfo["recipe"];
+      recipe = Recipe(
+        imgUrl: recipeInfo2["image"],
+        label: recipeInfo2["label"],
+        source: recipeInfo2["source"],
+        url: recipeInfo2["url"],
+      );
+      //recipe = Recipe.fromMap(recipeInfo["recipe"]);
       recipes.add(recipe);
-     });
+    });
 
-     setState(() {
-       
-     });
-
+    setState(() {
+      loading = false;
+    });
+    
   }
 
   @override
   void initState() {
-    getRecipes();
     super.initState();
   }
 
@@ -51,14 +63,116 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Recipe App'),
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+              child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('What would you like to cook?', style: TextStyle(fontSize: 18,),),
+              SizedBox(height: 10,),
+              Text("Enter Ingredients you have 😁", style: TextStyle(fontSize: 18),),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {
+                        ingredient = value;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Enter ingredient',
+                        labelText: 'Search'
+                      ),
+                    ),
+                  ),
+
+                  InkWell(
+                    onTap: () {
+                      if(ingredient != "") {
+                        recipes = [];
+                        getRecipes();
+                        setState(() {
+                          
+                        });
+                      }
+                    },
+                    child: Icon(Icons.search)
+                    ),
+                ],
+              ),
+
+              loading ? Container(
+                height: MediaQuery.of(context).size.height - 200,
+                child: Center(child: CircularProgressIndicator()),
+              ) : 
+              GridView(
+                physics: ClampingScrollPhysics(),
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  mainAxisSpacing: 10.0,
+                  crossAxisSpacing: 10.0
+                ),
+                children: List.generate(recipes.length, (index) {
+                        return RecipeTile(
+                          imgUrl: recipes[index].imgUrl,
+                          label: recipes[index].label,
+                          source: recipes[index].source,
+                          url: recipes[index].url,
+                        );
+                      }),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RecipeTile extends StatelessWidget {
+  final String label, source, imgUrl, url;
+  RecipeTile({this.url, this.imgUrl, this.label, this.source});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => RecipeView(url)));
+      },
+      child: Container(
+        child: Stack(
           children: [
-            Text('What would you like to cook?', style: TextStyle(fontSize: 18,),),
-            SizedBox(height: 10,),
-            Text("Enter Ingredients you have 😁", style: TextStyle(fontSize: 18),),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(imgUrl)),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    label,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    source,
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/views/recipe_view.dart
+++ lib/views/recipe_view.dart
@@ -1,16 +1,32 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class RecipeView extends StatefulWidget {
+  final String url;
+  RecipeView(this.url);
+
   @override
   _RecipeViewState createState() => _RecipeViewState();
 }
 
 class _RecipeViewState extends State<RecipeView> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WebView"),
+      ),
+      body: WebView(
+        initialUrl: widget.url,
+         onWebViewCreated: ((WebViewController webViewController) {
+          _completer.complete(webViewController);
+        }),
+      ),
     );
   }
 }
